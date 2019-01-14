9b93aac24cbd373910e4747828ebe9869af58c43
adamjorycromer <adamjorycromer@gmail.com>
2019-01-13 22:55:34 -0700

    updated hover links some media queries

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    var gitIcon = $("#gitIcon");
+    var linkedIcon = $("#linkedIcon");
+    var emailIcon = $("#emailIcon");
+
+    gitIcon.mouseenter(function () {
+        gitIcon.attr("src", "assets/images/gitgreen.png");
+    });
+    gitIcon.mouseleave(function () {
+        gitIcon.attr("src", "assets/images/gitgrey.png");
+    });
+
+    linkedIcon.mouseenter(function () {
+        linkedIcon.attr("src", "assets/images/linkgreen.png");
+    });
+    linkedIcon.mouseleave(function () {
+        linkedIcon.attr("src", "assets/images/linkgrey.png");
+    });
+
+    emailIcon.mouseenter(function () {
+        emailIcon.attr("src", "assets/images/emailgreen.png");
+    });
+    emailIcon.mouseleave(function () {
+        emailIcon.attr("src", "assets/images/emailgrey.png");
+    });
+
+
+
+});
